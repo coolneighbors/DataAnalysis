860f865356a2247542438e1f5518e0a803e4f7da
Noah Schapera <noahschapera.9@gmail.com>
2022-06-28 11:43:34 -0700

    Working data parser for known r/f subject sets

--- DataParser.py
+++ DataParser.py
@@ -7,6 +7,7 @@ Created on Mon Jun 27 13:35:23 2022
 
 import csv
 import json 
+import copy
 
 def stringToJson(string):
     res = json.loads(string)
@@ -19,38 +20,54 @@ def csvToDict(filename):
 
 def parseTargets(subject_file_string, class_file_string):
     
-    with open(subject_file_string, mode='r') as sub_file:
-        subjects = csv.DictReader(sub_file)
-        with open(class_file_string, mode='r') as class_file:
-            classifications = csv.DictReader(class_file)
-            for sub in subjects:
-                subID=sub["subject_id"]
-                metadata_json = stringToJson(sub["metadata"])
-                subClass = metadata_json["#R/F"]
-                if subClass == "Y":
-                    subBool = True
+    sub_file = open(subject_file_string, mode='r')
+
+    class_file = open(class_file_string, mode='r')
+    
+    subjects = csv.DictReader(sub_file)
+    classifications = csv.DictReader(class_file)
+    classifications_list = copy.copy(list(classifications))
+    
+    for sub in subjects:
+        subBool = None
+        subID=sub["subject_id"]
+        
+        metadata_json = stringToJson(sub["metadata"])
+        
+        subClass = metadata_json["#R/F"]
+        
+        if subClass == "R":
+            subBool = True
+        else:
+            subBool = False
+            
+            
+        correctClass=0
+        incorrectClass=0
+        
+        
+        for cl in classifications_list:           
+            classID = cl["subject_ids"]
+            
+            if classID == subID:
+                
+                anno = stringToJson(cl["annotations"])
+                
+                if anno[0]["value"] == "Yes":
+                    clBool = True
                 else:
-                    subBool = False
-                correctClass=0
-                incorrectClass=0
-                for cl in classifications:
-                    classID = cl["subject_ids"]
-                    print("sub id: " + subID)
-                    print("class id: " + classID)
-                    if classID == subID:
-                        print("Match")
-                        anno = stringToJson(cl["annotations"])
-                        if anno[0]["value"] == "Yes":
-                            clBool = False
-                        else:
-                            clBool = True
-                            
-                        if subBool == clBool:
-                            correctClass += 1
-                        else:
-                            incorrectClass += 1
-                        
-                print(f"Subject {subID} was classified correctly {correctClass} times and incorrect {incorrectClass} times")
+                    clBool = False
+                    
+                if subBool == clBool:
+                    correctClass += 1
+                else:
+                    incorrectClass += 1
+                
+        print(f"Subject {subID} was classified correctly {correctClass} times and incorrect {incorrectClass} times")
+    
+    
+    class_file.close()
+    sub_file.close()
                 
 parseTargets("byw-cn-test-project-subjects.csv","byw-cn-test-project-classifications.csv")
         
