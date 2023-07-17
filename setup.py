7f0b4b5049247c6156eff7149fb4685cf132e274
austinh2001 <austinh2001@gmail.com>
2023-07-17 15:05:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ if(len(python_magic) != 1):
         raise Exception(f'python-magic requirement not found in requirements.txt or found multiple times: \'{python_magic}\'')
 python_magic = python_magic[0]
 python_magic_version = python_magic.split('==')[1]
+
 if(len(python_magic_version) != 0):
     subprocess.call(['pip', 'install', f'python-magic=={python_magic_version}'])
 else:
@@ -21,6 +22,7 @@ else:
 python_magic_bin = [line for line in requirements if 'python-magic-bin' in line]
 if(len(python_magic_bin) != 1):
     raise Exception(f'python-magic-bin requirement not found in requirements.txt or found multiple times: \'{python_magic_bin}\'')
+
 python_magic_bin = python_magic_bin[0]
 python_magic_bin_version = python_magic_bin.split('==')[1]
 if(len(python_magic_bin_version) != 0):
@@ -45,14 +47,3 @@ setup(
     description='The Cool Neighbors data analysis GitHub repository.',
     long_description=open('README.md').read(),
 )
-
-# This fixes an import bug with the python-magic and python-magic-bin packages.
-# https://github.com/zooniverse/panoptes-python-client/issues/264
-python_magic_bin = [line for line in requirements if 'python-magic-bin' in line]
-if(len(python_magic_bin) != 1):
-    raise Exception('python-magic-bin requirement not found in requirements.txt or found multiple times.')
-python_magic_bin = python_magic_bin[0]
-python_magic_bin_version = python_magic_bin.split('==')[1]
-subprocess.call(['pip', 'uninstall', f'python-magic-bin=={python_magic_bin_version}'])
-subprocess.call(['pip', 'install', f'python-magic-bin=={python_magic_bin_version}'])
-
