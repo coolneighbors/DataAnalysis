dbfcca7190d93a6c065af5c5e74b1bda52028a7e
austinh2001 <austinh2001@gmail.com>
2023-07-17 14:41:49 -0400

    Potential solution to python-magic import bug

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import subprocess
 from setuptools import setup
 
 with open('requirements.txt', encoding="utf-16") as f:
@@ -15,3 +16,9 @@ setup(
     description='The Cool Neighbors data analysis GitHub repository.',
     long_description=open('README.md').read(),
 )
+
+# This fixes an import bug with the python-magic and python-magic-bin packages.
+# https://github.com/zooniverse/panoptes-python-client/issues/264
+subprocess.call(['pip', 'uninstall', 'python-magic-bin==0.4.14'])
+subprocess.call(['pip', 'install', 'python-magic-bin==0.4.14'])
+
