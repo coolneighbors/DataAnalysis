54acd3a99cde9bf61c69981217512ac0d04851d0
austinh2001 <austinh2001@gmail.com>
2023-07-17 14:57:10 -0400

    Further attempt to fix python-magic import issue

--- setup.py
+++ setup.py
@@ -4,6 +4,31 @@ from setuptools import setup
 with open('requirements.txt', encoding="utf-16") as f:
     requirements = f.read().splitlines()
 
+python_magic = [line for line in requirements if 'python-magic' in line]
+if(len(python_magic) != 1):
+    raise Exception('python-magic requirement not found in requirements.txt or found multiple times.')
+python_magic = python_magic[0]
+python_magic_version = python_magic.split('==')[1]
+if(len(python_magic_version) != 0):
+    subprocess.call(['pip', 'install', f'python-magic=={python_magic_version}'])
+else:
+    subprocess.call(['pip', 'install', 'python-magic'])
+
+python_magic_bin = [line for line in requirements if 'python-magic-bin' in line]
+if(len(python_magic_bin) != 1):
+    raise Exception('python-magic-bin requirement not found in requirements.txt or found multiple times.')
+python_magic_bin = python_magic_bin[0]
+python_magic_bin_version = python_magic_bin.split('==')[1]
+if(len(python_magic_bin_version) != 0):
+    subprocess.call(['pip', 'uninstall', f'python-magic-bin=={python_magic_bin_version}'])
+    subprocess.call(['pip', 'install', f'python-magic-bin=={python_magic_bin_version}'])
+else:
+    subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
+    subprocess.call(['pip', 'install', 'python-magic-bin'])
+
+requirements.remove(python_magic)
+requirements.remove(python_magic_bin)
+
 setup(
     name='DataToolkit',
     url='https://github.com/coolneighbors/DataToolkit.git',
@@ -19,6 +44,11 @@ setup(
 
 # This fixes an import bug with the python-magic and python-magic-bin packages.
 # https://github.com/zooniverse/panoptes-python-client/issues/264
-subprocess.call(['pip', 'uninstall', 'python-magic-bin==0.4.14'])
-subprocess.call(['pip', 'install', 'python-magic-bin==0.4.14'])
+python_magic_bin = [line for line in requirements if 'python-magic-bin' in line]
+if(len(python_magic_bin) != 1):
+    raise Exception('python-magic-bin requirement not found in requirements.txt or found multiple times.')
+python_magic_bin = python_magic_bin[0]
+python_magic_bin_version = python_magic_bin.split('==')[1]
+subprocess.call(['pip', 'uninstall', f'python-magic-bin=={python_magic_bin_version}'])
+subprocess.call(['pip', 'install', f'python-magic-bin=={python_magic_bin_version}'])
 
