a4270869471e13cc6f66489f991ffe4577e82d91
austinh2001 <austinh2001@gmail.com>
2023-07-25 12:55:11 -0400

    Update __init__.py

--- DataToolkit/__init__.py
+++ DataToolkit/__init__.py
@@ -2,6 +2,7 @@ import subprocess
 
 # This fixes an import bug with the python-magic and python-magic-bin packages.
 # https://github.com/zooniverse/panoptes-python-client/issues/264
+subprocess.call(['pip', 'install', 'python-magic'])
 subprocess.call(['pip', 'uninstall', 'python-magic-bin'])
 subprocess.call(['pip', 'install', 'python-magic-bin'])
 
